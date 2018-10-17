d08b1b038fbc516625f7c2fb76022862ac342e57
REVE <kgw1988@naver.com>
2018-10-18 00:27:07 +0900

    first commit

---
+++ src/main/java/kr/revelope/jdk11study/Jdk11studyApplication.java
@@ -0,0 +1,12 @@
+package kr.revelope.jdk11study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk11studyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk11studyApplication.class, args);
+    }
+}
---
+++ src/test/java/kr/revelope/jdk11study/Jdk11studyApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.revelope.jdk11study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jdk11studyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
